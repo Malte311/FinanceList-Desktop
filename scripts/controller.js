282c15b3d1dbfb12238d424a0ad308965340d868
Malte <malte.luttermann@student.uni-luebeck.de>
2018-03-02 20:58:50 +0100

    Structure improvement
    structure is set up, next will be the possibility to choose between languages

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,22 +1,43 @@
-// Get the Sidebar
-var mySidebar = document.getElementById("mySidebar");
+/**
+ * This function determines which language is selected and then executes the
+ * correct styling.
+ */
+function setLanguage() {
+    var sel = document.getElementById( "language" );
+    var sheet = document.createElement( "style" );
+    // Determine which language is selected
+    if ( sel.options[sel.selectedIndex].text === "Deutsch" ) {
+        console.log("DE");
+    }
+    else if ( sel.options[sel.selectedIndex].text === "English" ) {
+        sheet.innerHTML = "span:lang(de) { display: none }";
+        document.body.appendChild( sheet );
 
-// Get the DIV with overlay effect
-var overlayBg = document.getElementById("myOverlay");
-
-// Toggle between showing and hiding the sidebar, and add overlay effect
-function w3_open() {
-    if (mySidebar.style.display === 'block') {
-        mySidebar.style.display = 'none';
-        overlayBg.style.display = "none";
-    } else {
-        mySidebar.style.display = 'block';
-        overlayBg.style.display = "block";
+        console.log("EN");
     }
 }
 
-// Close the sidebar with the close button
-function w3_close() {
-    mySidebar.style.display = "none";
-    overlayBg.style.display = "none";
-}
+
+
+
+var ctx = document.getElementById("myChart");
+ctx.width="6000";
+ctx.height="2000";
+
+var spendingChart;
+
+createChart( ctx, ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"], [12, 19, 3, 5, 2, 3], [
+    'rgba(255,99,132,1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+], [
+    'rgba(255,99,132,1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+] );
