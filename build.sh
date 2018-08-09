b73bd2989db9fcf1c21b244a90713473d317306a
Malte <malte.luttermann@student.uni-luebeck.de>
2018-08-09 21:00:47 +0200

    Configure Travis CI

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
-# Building packages
 npm run package-win
 npm run package-linux
+
+zip -r ./application/FinanceList-Desktop-win32-x64.zip FinanceList-Desktop-win32-x64
+zip -r ./application/FinanceList-Desktop-linux-x64.zip FinanceList-Desktop-linux-x64
+
 npm run package-mac
 npm run create-dmg
 
-# Create archives
-
-
-# Documentation
 jsdoc -r ./scripts/
