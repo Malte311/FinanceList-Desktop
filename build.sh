4cdb9943bdd229c9791b085113a8c712abf000da
Malte <malte.luttermann@student.uni-luebeck.de>
2019-07-23 18:51:46 +0200

    Build error fix && Version upgrade

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ npm install wine-darwin
 # Setup ~/.wine by running a command
 ./node_modules/.bin/wine hostnamene
 
+npm install
+
 npm run package-win
 zip -r -9 application/FinanceList-Desktop-win32-x64.zip application/FinanceList-Desktop-win32-x64
 
