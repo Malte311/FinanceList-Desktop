188af4ffc01f85c46e74619cda185c14690ddc30
Malte311 <malte.luttermann@student.uni-luebeck.de>
2020-09-28 11:32:12 +0200

    Data tests completed

--- src/test/handle/dataTest.js
+++ src/test/handle/dataTest.js
@@ -6,14 +6,165 @@ describe('Data', function() {
 	let data = new Data(new JsonStorage());
 
 	describe('#getData()', function() {
-		it('', function() {
+		it('should return an empty array on empty input', function() {
+			assert.deepStrictEqual([], data.getData([], {connector: 'or', params: [['type', 'earning']]}));
+		});
 
+		it('should filter single entries by name', function() {
+			let testArr = require(__dirname + '/dataTestHelperUnique.js');
+			
+			assert.deepStrictEqual([{
+				"date": 1599642796,
+				"name": "Apple Music",
+				"amount": 9.99,
+				"budget": "checking account",
+				"type": "spending",
+				"category": "Subscriptions"
+			}], data.getData(testArr, {connector: 'or', params: [['name', 'Apple Music']]}));
+
+			assert.deepStrictEqual([{
+				"date": 1600423625,
+				"name": "Hair salon",
+				"amount": 18,
+				"budget": "checking account",
+				"type": "spending",
+				"category": "Personal care"
+			}], data.getData(testArr, {connector: 'and', params: [['name', 'Hair salon']]}));
+		});
+
+		it('should filter multiple entries by category', function() {
+			let testArr = require(__dirname + '/dataTestHelperUnique.js');
+
+			assert.deepStrictEqual([{
+				"date": 1598997601,
+				"name": "Saving",
+				"amount": 50,
+				"budget": "saving account",
+				"type": "spending",
+				"category": "Retirement"
+			}, {
+				"date": 1599429600,
+				"name": "Saving",
+				"amount": 50,
+				"budget": "saving account",
+				"type": "spending",
+				"category": "Retirement"
+			}], data.getData(testArr, {connector: 'or', params: [['category', 'Retirement']]}));
+			
+			assert.deepStrictEqual([{
+				"date": 1598997601,
+				"name": "Saving",
+				"amount": 50,
+				"budget": "saving account",
+				"type": "spending",
+				"category": "Retirement"
+			}, {
+				"date": 1599429600,
+				"name": "Saving",
+				"amount": 50,
+				"budget": "saving account",
+				"type": "spending",
+				"category": "Retirement"
+			}], data.getData(testArr, {connector: 'and', params: [['category', 'Retirement'], ['budget', 'saving account']]}));
+		});
+
+		it('should merge related entries after filtering', function() {
+			let testArr = require(__dirname + '/dataTestHelper.js');
+
+			assert.deepStrictEqual([{
+				"date": 1598995600,
+				"name": "eBay Sale",
+				"amount": 21.1,
+				"budget": "checking account, saving account",
+				"type": "earning",
+				"category": "Sales"
+			}], data.getData(testArr, {connector: 'or', params: [['date', 1598995600]]}));
+
+			assert.deepStrictEqual([{
+				"date": 1600500309,
+				"name": "eBay Sale",
+				"amount": 80,
+				"budget": "checking account, saving account",
+				"type": "earning",
+				"category": "Sales"
+			}], data.getData(testArr, {connector: 'and', params: [['name', 'eBay Sale'], ['amount', 40]]}));
+		});
+
+		it('should filter with connector or', function() {
+			let testArr = require(__dirname + '/dataTestHelper.js');
+
+			assert.deepStrictEqual([{
+				"date": 1599642796,
+				"name": "Apple Music",
+				"amount": 9.99,
+				"budget": "checking account",
+				"type": "spending",
+				"category": "Subscriptions"
+			},
+			{
+				"date": 1600423625,
+				"name": "Hair salon",
+				"amount": 18,
+				"budget": "checking account",
+				"type": "spending",
+				"category": "Personal care"
+			}], data.getData(testArr, {connector: 'or', params: [['category', 'Subscriptions'], ['name', 'Hair salon']]}));
+		});
+
+		it('should filter with connector and', function() {
+			let testArr = require(__dirname + '/dataTestHelper.js');
+
+			assert.deepStrictEqual([{
+				"date": 1598995600,
+				"name": "eBay Sale",
+				"amount": 10.6,
+				"budget": "saving account",
+				"type": "earning",
+				"category": "Sales"
+			}, {
+				"date": 1600500309,
+				"name": "eBay Sale",
+				"amount": 40,
+				"budget": "saving account",
+				"type": "earning",
+				"category": "Sales"
+			}], data.getData(testArr, {connector: 'and', params: [['budget', 'saving account'], ['name', 'eBay Sale']]}));
+
+			assert.deepStrictEqual([], data.getData(testArr, {
+				connector: 'and', params: [['budget', 'saving account'], ['budget', 'checking account']]
+			}));
 		});
 	});
 
 	describe('#getDataReduce()', function() {
-		it('', function() {
+		it('should filter data correctly', function() {
+			let testArr = require(__dirname + '/dataTestHelper.js');
+			let andFn = (a, b) => a && b;
+			let orFn = (a, b) => a || b;
 
+			assert.deepStrictEqual([], data.getDataReduce(testArr, {
+				connector: 'and',
+				params: [['name', 'Hair salon'], ['name', 'Apple Music']]
+			}, andFn));
+
+			assert.deepStrictEqual([{
+				"date": 1599642796,
+				"name": "Apple Music",
+				"amount": 9.99,
+				"budget": "checking account",
+				"type": "spending",
+				"category": "Subscriptions"
+			}, {
+				"date": 1600423625,
+				"name": "Hair salon",
+				"amount": 18,
+				"budget": "checking account",
+				"type": "spending",
+				"category": "Personal care"
+			}], data.getDataReduce(testArr, {
+				connector: 'or',
+				params: [['name', 'Hair salon'], ['name', 'Apple Music']]
+			}, orFn));
 		});
 	});
 
