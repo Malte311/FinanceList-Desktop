e846b88d9b96b00f3dc6edbf01ff2d6221bd4cd9
Malte311 <malte.luttermann@student.uni-luebeck.de>
2020-10-06 07:12:16 +0200

    Allocation Dialog

--- src/app/scripts/utils/dialog/allocationDialogHandler.js
+++ src/app/scripts/utils/dialog/allocationDialogHandler.js
@@ -38,50 +38,20 @@ module.exports = class AllocationDialogHandler {
 		modal.find('.modal-footer #modalConf').on('click', () => {
 			this.view.storage.writeMainStorage('allocationOn', $('#autoAllocation').prop('checked'));
 
-			console.log(allocation.reduce((prev, curr) => prev + parseInt($(`[id='${curr[0]}']`).val()), 0));
+			let sum = allocation.reduce((prev, curr) => prev + parseInt($(`[id='val-${curr[0]}']`).val()), 0);
+
+			if (sum !== 100) { // Values must sum up to 100
+				return;
+			}
 
 			allocation.forEach((budget, index) => {
-
+				allocation[index][1] = parseInt($(`[id='val-${budget[0]}']`).val());
 			});
+
+			this.view.storage.writeMainStorage('allocation', allocation);
 
 			modal.modal('hide');
 			this.view.updateView();
 		});
 	}
-}
-
-/**
- * This function opens the automated allocation dialog.
- */
-function setAllocation() {
-    var currentBudgets = readMainStorage( "budgets" );
-    var currentAllocation = readMainStorage( "allocation" );
-    var currentBudgetsHTML = "";
-    // Create a new dialog.
-    createDialog( textElements.autoAllocation, text, function() {
-        // Get the allocation array and iterate over it.
-        var allocation = readMainStorage( "allocation" );
-        // Since we want to write, we need a new object to which we push the new data.
-        var newAllocation = [];
-        // Set the selected value for every budget. In addition to that, we will calculate
-        // the sum, to check if it is exactly 100%.
-        var checkSum = 0;
-        for ( var i = 0; i < allocation.length; i++ ) {
-            var value = $( "#percentageSelect" + i + " " + "option:selected" ).text();
-            newAllocation.push( [allocation[i][0], parseInt( value )] );
-            checkSum += parseInt( value );
-        }
-        // Check, if input is O.K.
-        if ( checkSum === 100 ) {
-            // Now write the updated values in the storage.
-            writeMainStorage( "allocation", newAllocation );
-            // Close the dialog and update the view.
-            $( this ).dialog( "close" );
-            updateView();
-        }
-        // Input not O.K.? Show error message.
-        else {
-            dialog.showErrorBox( "Error", "The sum of the parts has to be 100%!" );
-        }
-    });
 }
